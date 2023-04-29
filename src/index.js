a290bc4e7200993164c72a592857e7d8668676c7
Igor <Igorcbraz1@gmail.com>
2023-04-29 11:32:23 -0300

    :bug: FIX: eslint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './App.jsx';
+// eslint-disable-next-line import/extensions
+import App from './App.jsx'
 
-import 'aos/dist/aos.css';
+import 'aos/dist/aos.css'
 import './styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
